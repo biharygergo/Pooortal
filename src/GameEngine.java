48828c6e99fb9c4d668d98e51d366656af8f562b
Gergo <biharygergo@gmail.com>
2016-03-18 19:08:15 +0100

    Initial commit

---
+++ src/GameEngine.java
@@ -0,0 +1,70 @@
+/**
+ * Created by Gergo on 18/03/16.
+ */
+package src;
+import java.util.*;
+
+/**
+ *
+ */
+public class GameEngine {
+
+    /**
+     * Default constructor
+     */
+    public GameEngine() {
+    }
+
+    /**
+     *
+     */
+   // private Player oNeill;
+
+    /**
+     *
+     */
+    //private Modules activeModules;
+
+    /**
+     *
+     */
+   // private FieldMap map;
+
+    /**
+     *
+     */
+   // private Wormhole wormhole;
+
+    /**
+     *
+     */
+
+
+
+
+
+
+
+
+    /**
+     * @return
+     */
+    public void start() {
+        // TODO implement here
+
+    }
+
+    /**
+     * @return
+     */
+    public void endGame() {
+        // TODO implement here
+
+    }
+
+    public static void main(String[] args){
+        System.out.println("It Works!");
+
+    }
+
+}
