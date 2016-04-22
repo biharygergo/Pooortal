26cd30fe54560d3d62fb69cdf8d7fb9c10322308
Gergo <biharygergo@gmail.com>
2016-04-22 15:33:57 +0200

    Keszulnek a logikak...

--- src/src/Player.java
+++ src/src/Player.java
@@ -98,12 +98,7 @@ public class Player {
      * @return The field which the player currently stands on
      */
     public Field getField() {
-        // TODO implement here
-        GameEngine.tab++;
-        GameEngine.print("Player.getField - Returned field");
 
-
-        GameEngine.tab--;
         return field;
     }
 
@@ -111,117 +106,61 @@ public class Player {
      * @param field The field to be set for the player
      */
     public void setField(Field field) {
-        // TODO implement here
-        GameEngine.tab++;
-        GameEngine.print("Player.setField - Field set");
         this.field=field;
-        GameEngine.tab--;
     }
 
     /**
      * @return The direction of the player
      */
     public Dir getDir() {
-        // TODO implement here
-        GameEngine.tab++;
-        GameEngine.print("Player.getDir - Returned direction");
-        GameEngine.tab--;
-        return Dir.Down;
+       return dir;
     }
 
     /**
      * @param dir The direction to be set for the player
      */
     public void setDir(Dir dir) {
-        // TODO implement here
-        GameEngine.tab++;
-        GameEngine.print("Player.setDir - Direction set");
-        GameEngine.tab--;
 
+      this.dir = dir;
     }
 
     /**
      * @return The box which the player has picked up
      */
     public Box getBox() {
-        // TODO implement here
-        GameEngine.tab++;
-        GameEngine.print("Player.getBox - Returned box");
-        GameEngine.print("Van a játékosnál doboz? Y / N");
-        Scanner scan=new Scanner(System.in);
-        String input=scan.nextLine().toUpperCase();
 
-        if(input.equals("Y")){
-            Box box=new Box();
-            GameEngine.tab--;
-            return box;
-        }
-        GameEngine.tab--;
-        return null;
+        return box;
     }
 
     /**
      * @param box The box to be set for the player
      */
     public void setBox(Box box) {
-        // TODO implement here
-        GameEngine.tab++;
-        GameEngine.print("Player.setBox - Box set");
-        GameEngine.tab--;
+        this.box = box;
     }
 
     /**
      * @return Whether the player is alive or not
      */
     public boolean isAlive() {
-        // TODO implement here
-        GameEngine.tab++;
-        GameEngine.print("Player.isAlive - Returned player state");
-        GameEngine.print("Meghaltunk? Y/N");
-        Scanner scan=new Scanner(System.in);
-        String line=scan.nextLine().toUpperCase();
-
-        GameEngine.tab--;
-        if (line.equals("Y"))
-            return false;
-        else return true;
-
-
-
+        return alive;
     }
 
     /**
      * @param alive Set if the player is alive or dead
      */
     public void setAlive(boolean alive) {
-        // TODO implement here
-        GameEngine.tab++;
-        GameEngine.print("Player.setAlive - Player state set");
-        GameEngine.tab--;
+        this.alive = alive;
     }
 
     /**
      * @return The next field
      */
     public Field getNextField() {
-        // TODO implement here
-        GameEngine.tab++;
-        GameEngine.print("Player.getNextField - Returned next field");
 
-        GameEngine.print("Lehet a következő mezőre lépni? Y / N");
-        Scanner scan=new Scanner(System.in);
+        Field nextField = field.getSide(dir);
 
-       String input= scan.nextLine().toUpperCase();
-        if(input.equals("Y")) {
-            Road r=new Road();
-            GameEngine.tab--;
-            return r;
-        }
-        else {
-            Wall r=new Wall();
-            GameEngine.tab--;
-            return r;
-        }
+        return nextField;
 
     }
 
