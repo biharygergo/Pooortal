1a1057a7d7fcfd77400a5e365205d8d4a75f37ec
Gergo <biharygergo@gmail.com>
2016-04-23 22:49:29 +0200

    Keszulnek a logikak...

--- src/src/Door.java
+++ src/src/Door.java
@@ -19,7 +19,9 @@ public class Door extends Field {
      */
     private boolean open;
 
+    private Scale scale;
 
+    private boolean playerBlockingDoor;
     /**
      * @return Returns true if the door is open, false otherwise
      */
@@ -42,10 +44,30 @@ public class Door extends Field {
      * @param player the player stepped on the door
      */
     public void onStep(Player player) {
-
+//TODO doksiba írni: uj változó doorblocking? private mondjuk de a fv public!
+        playerBlockingDoor=true;
       //GameEngine beallitja hogy mi legyen, itt ez sima Road-kent funkcional!
     }
 
+    /**
+     * Returns if there is a player blocking the door
+     * @return
+     */
+    public boolean isPlayerBlockingDoor(){
+        return playerBlockingDoor;
+    }
+
+    public void reCheckWeightAfterSteppingOff(){
+
+        scale.reCheckWeights();
+    }
+    /**
+     * Set if player is still blocking
+     * @param block
+     */
+    public void setPlayerBlockingDoor(boolean block){
+        playerBlockingDoor = block;
+    }
     /**
      * @return Returns true if open, false otherwise
      */
@@ -79,12 +101,13 @@ public class Door extends Field {
         return sides;
     }
 
-    /**
-     * Sets the new neighbours
-     * @param map the neighbours of the door in the specified direction
-     */
+   public void setScale(Scale scale){
+       this.scale = scale;
+   }
 
-
+    public Scale getScale(){
+        return scale;
+    }
     /**
      * What happens on replicator step
      * @param replicator this is the replicator that stepped on the door
