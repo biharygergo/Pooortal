78c57dac6abb733349a2702b9f8a641d7b921431
Knight55 <toth.dani9204@gmail.com>
2016-05-16 15:20:53 +0200

    clean

--- src/src/View.java
+++ src/src/View.java
@@ -7,15 +7,13 @@ import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.List;
 
 public class View extends JFrame {
 
     private JPanel contentPane;
-    //private JLabel[][] labels = new JLabel[6][6];
     public static View frame;
     private int Fwidth = 50, Fheight = 50;
-    //private  Graphics g;
+
     /**
      * Launch the application.
      * @param controller
@@ -24,7 +22,7 @@ public class View extends JFrame {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
-                     frame = new View();
+                    frame = new View();
 
                     frame.addKeyListener(controller);
                     Controller.loadImages();
@@ -36,12 +34,13 @@ public class View extends JFrame {
         });
     }
 
-    public void paint(Graphics g){
-
-    }
+    /**
+     *
+     * @param g
+     */
+    public void paint(Graphics g){}
 
     public void Invalidate(){
-
 
         Graphics g = getGraphics();
         g.setColor(Color.lightGray);
@@ -80,6 +79,15 @@ public class View extends JFrame {
 
     }
 
+    /**
+     *
+     * @param originalImage
+     * @param width
+     * @param height
+     * @param type
+     * @return
+     * @throws IOException
+     */
     private BufferedImage resizeImage(BufferedImage originalImage, int width, int height, int type) throws IOException {
         BufferedImage resizedImage = new BufferedImage(width, height, type);
         Graphics2D g = resizedImage.createGraphics();
@@ -88,6 +96,12 @@ public class View extends JFrame {
         return resizedImage;
     }
 
+    /**
+     *
+     * @param bi
+     * @param x
+     * @param y
+     */
     public void paintOn(BufferedImage bi, int x, int y){
         Graphics g = getGraphics();
 
@@ -103,12 +117,20 @@ public class View extends JFrame {
 
     }
 
+    /**
+     *
+     * @param player
+     */
     public void setPlayerImage(Player player) {
         int y = player.getY();
         int x = player.getX();
         paintOn(player.getImage(), x, y);
     }
 
+    /**
+     *
+     * @param field
+     */
     public void setFieldImage(Field field){
         int y = field.getyPos();
         int x = field.getxPos();
@@ -116,6 +138,10 @@ public class View extends JFrame {
 
     }
 
+    /**
+     *
+     * @param box
+     */
     public void setBoxImage(Box box){
         int y = box.getField().getyPos();
         int x = box.getField().getxPos();
@@ -124,32 +150,47 @@ public class View extends JFrame {
 
     }
 
+    /**
+     *
+     * @param zpm
+     */
     public void setZPMImage(ZPM zpm){
         int y = zpm.getField().getyPos();
         int x = zpm.getField().getxPos();
         paintOn(zpm.getImage(), x, y);
     }
 
+    /**
+     *
+     * @param replicator
+     */
     public void setReplicatorImage(Replicator replicator){
         int y = replicator.getField().getyPos();
         int x = replicator.getField().getxPos();
         paintOn(replicator.getImage(), x, y);
     }
 
+    /**
+     *
+     * @param scale
+     */
     public void writeScaleWeight(Scale scale){
         int y = scale.getyPos();
         int x = scale.getxPos();
 
-
-            Graphics g = getGraphics();
-            g.setColor(Color.BLACK);
-            g.fillRect((y - 1) * Fheight + 14, (x - 1) * Fwidth + 16, 23, 18);
-            g.setColor(Color.WHITE);
-            g.drawString(String.valueOf(scale.getCurrentWeight()), (y - 1) * Fheight + 18, (x - 1) * Fwidth + 29);
+        Graphics g = getGraphics();
+        g.setColor(Color.BLACK);
+        g.fillRect((y - 1) * Fheight + 14, (x - 1) * Fwidth + 16, 23, 18);
+        g.setColor(Color.WHITE);
+        g.drawString(String.valueOf(scale.getCurrentWeight()), (y - 1) * Fheight + 18, (x - 1) * Fwidth + 29);
 
 
     }
 
+    /**
+     *
+     * @param field
+     */
     public void invalidateField(Field field){
         Graphics g = getGraphics();
         g.setColor(Color.lightGray);
@@ -157,7 +198,12 @@ public class View extends JFrame {
 
     }
 
-
+    /**
+     *
+     * @param image
+     * @param angle
+     * @return
+     */
     public static BufferedImage rotate(BufferedImage image, double angle) {
         double sin = Math.abs(Math.sin(angle)), cos = Math.abs(Math.cos(angle));
         int w = image.getWidth(), h = image.getHeight();
@@ -172,12 +218,21 @@ public class View extends JFrame {
         return result;
     }
 
+    /**
+     *
+     * @return
+     */
     private static GraphicsConfiguration getDefaultConfiguration() {
         GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         GraphicsDevice gd = ge.getDefaultScreenDevice();
         return gd.getDefaultConfiguration();
     }
 
+    /**
+     *
+     * @param dir
+     * @return
+     */
     public static double getAngleFromDir(Dir dir){
         double degree;
         switch (dir){
@@ -204,6 +259,10 @@ public class View extends JFrame {
         return Math.toRadians(degree);
     }
 
+    /**
+     *
+     * @param bulletImage
+     */
     public void setBulletImage(Bullet bulletImage) {
         int y = bulletImage.getField().getyPos();
         int x = bulletImage.getField().getxPos();
