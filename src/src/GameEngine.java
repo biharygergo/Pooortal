a3e7a1c4fda314e5aefef2fdf47bf11ff53cb0cc
Gergo <biharygergo@gmail.com>
2016-05-16 15:45:55 +0200

    fix: nincs nullpointerexception portalra rakott doboznal

--- src/src/GameEngine.java
+++ src/src/GameEngine.java
@@ -393,9 +393,11 @@ public class GameEngine {
                 Box box = oNeill.dropBox();
                 activeModules.checkBoxes();
 
-                if (box.getField().getDescription().equals("Scale")) {
-                    Scale scale = (Scale) box.getField();
-                    AnimateOneField(scale.getDoor());
+                if(box!=null) {
+                    if (box.getField().getDescription().equals("Scale")) {
+                        Scale scale = (Scale) box.getField();
+                        AnimateOneField(scale.getDoor());
+                    }
                 }
             }
         }
@@ -425,9 +427,11 @@ public class GameEngine {
                 Box box = Jaffa.dropBox();
                 activeModules.checkBoxes();
 
-                if (box.getField().getDescription().equals("Scale")) {
-                    Scale scale = (Scale) box.getField();
-                    AnimateOneField(scale.getDoor());
+                if(box!=null) {
+                    if (box.getField().getDescription().equals("Scale")) {
+                        Scale scale = (Scale) box.getField();
+                        AnimateOneField(scale.getDoor());
+                    }
                 }
             }
 
