74c8796ec70050b95b90e010b6522f7fc6108a46
Knight55 <toth.dani9204@gmail.com>
2016-05-16 17:15:12 +0200

    clean

--- src/src/Main.java
+++ src/src/Main.java
@@ -6,14 +6,14 @@ package src;
 public class Main {
 
     private static GameEngine gameEngine = new GameEngine();
-    private static Controller controller = new Controller(gameEngine,"listener");
-    private static Controller controllerOnNewThread = new Controller(gameEngine,"updater");
+    private static Controller controller = new Controller(gameEngine, "listener");
+    private static Controller controllerOnNewThread = new Controller(gameEngine, "updater");
+
     public static void main (String[] args){
 
         Thread t = new Thread(controller);
         Thread t2 = new Thread(controllerOnNewThread);
         t.start();
         t2.start();
-
     }
 }
